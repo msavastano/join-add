6fdbf022df458fc747c5f1a71c02d2b707eceaa6
msavast1 <msavast1@its.jnj.com>
2020-06-18 22:42:07 -0400

    added tests

--- test.js
+++ test.js
@@ -1,28 +1,38 @@
-const join = require('./index')
+const joinAnd = require('./index')
 const chai = require('chai');
 const { expect } = chai;
 
 describe('Join-and tests', () => {
   const array = ['a', 'b', 'c'];
   it('should return join all array elements into a string with "and"', () => {
-    expect(join(array, ', ', ' and ')).to.equal('a, b and c')
-    expect(join(array, ', ', 'and')).to.equal('a, bandc')
-    expect(join(array, ', ', '')).to.equal('a, bc')
-    expect(join(array, ', ', ' ')).to.equal('a, b c')
+    expect(joinAnd(array, ', ', ' and ')).to.equal('a, b and c')
+    expect(joinAnd(array, ', ', 'and')).to.equal('a, bandc')
+    expect(joinAnd(array, ', ', '')).to.equal('a, bc')
+    expect(joinAnd(array, ', ', ' ')).to.equal('a, b c')
+    expect(joinAnd(array, ', ', null)).to.equal('a, b, c')
+    expect(joinAnd(array, null, null)).to.equal('a,b,c')
   })
 
   it('should return join all array elements into a string with native join', () => {
-    expect(join(array, '~')).to.equal('a~b~c')
-    expect(join(array)).to.equal('a,b,c')
+    expect(joinAnd(array, '~')).to.equal('a~b~c')
+    expect(joinAnd(array)).to.equal('a,b,c')
   })
 
   it('should return join all array elements into a string with null separator', () => {
-    expect(join(array, null)).to.equal('a,b,c')
-    expect(join(array, null, ' and ')).to.equal('a,b and c')
+    expect(joinAnd(array, null)).to.equal('a,b,c')
+    expect(joinAnd(array, null, ' and ')).to.equal('a,b and c')
   })
 
   it('should return join all array elements into a string with less than 3 elements', () => {
     const partial = array.slice(1)
-    expect(join(partial, ', ', ' and ')).to.equal('b, c')
+    expect(joinAnd(partial, ', ', ' and ')).to.equal('b, c')
+  })
+
+  it('should return join all array elements into a string on different array cases', () => {
+    expect(joinAnd([null], ', ', ' and ')).to.equal('')
+    expect(joinAnd(['a'], ', ', ' and ')).to.equal('a')
+    expect(joinAnd(null, ', ', ' and ')).to.equal('')
+    expect(joinAnd(undefined, ', ', ' and ')).to.equal('')
+    expect(joinAnd([undefined], ', ', ' and ')).to.equal('')
   })
 })
